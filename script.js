29fe6f8decb184b6f5fe761ad8455b44c9a48528
Zehno-Work <126801501+Zehnooo@users.noreply.github.com>
2025-08-20 11:02:02 -0500

    put all functions in an operations object

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const getSum = (x, y) => x + y;
-const getDifference = (x, y) => x - y;
-const getProduct = (x, y) => x * y;
-const getQuotient = (x, y) => x / y;
+let numX;
+let numY;
+let operator;
+
+const operations = {
+  add: function getSum(x, y) {
+    return x + y;
+  },
+
+  subtract: function getDifference(x, y) {
+    return x - y;
+  },
+
+  multiply: function getProduct(x, y) {
+    return x * y;
+  },
+
+  divide: function getQuotient(x, y) {
+    return x / y;
+  },
+};
+
+function operate(x, y, op) {}
