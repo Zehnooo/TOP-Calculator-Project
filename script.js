a65bfa7a11850a5506364a134acf1d862535e3fb
Zehno-Work <126801501+Zehnooo@users.noreply.github.com>
2025-08-20 13:16:35 -0500

    added event listeners to buttons and IDs to buttons that are operations

--- script.js
+++ script.js
@@ -24,3 +24,14 @@ function operate(x, y, op) {
   let operation = operations[op];
   return operation(x, y);
 }
+
+function useButton(btn) {
+  value = btn.textContent;
+  btn.id ? alert(btn.id) : alert("no button Id set" + value);
+}
+
+const buttonContainer = document.querySelector(".calc-buttons");
+const buttons = buttonContainer.querySelectorAll("button");
+for (const btn of buttons) {
+  btn.addEventListener("click", () => useButton(btn));
+}
