a2237fe355787d65ae4e57d458e1274b75a0664a
Zehno-Work <126801501+Zehnooo@users.noreply.github.com>
2025-08-21 13:35:32 -0500

    Added element to store last selected number
    add arrays for correct operators and arithmetic functions to utilize switch statements to call proper function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let numX;
 let numY;
 let operator;
+const validOperators = ["add", "subtract", "multiply", "divide"];
+const validFunctions = ["clear", "pos-neg", "percent", "operate"];
+
+const calcDisplay = document.querySelector("#calc-num");
+const calcSavedNum = document.querySelector("#calc-num-saved");
+// grab calc buttons and add event listener
+const buttonContainer = document.querySelector(".calc-buttons");
+const buttons = buttonContainer.querySelectorAll("button");
+for (const btn of buttons) {
+  btn.addEventListener("click", () => useButton(btn));
+}
 
 const operations = {
   add: function getSum(x, y) {
@@ -26,21 +37,49 @@ function operate(x, y, op) {
 }
 
 function useButton(btn) {
-  const calcDisplay = document.querySelector("#calc-num");
-  const value = btn.textContent;
-  if (isNaN(value)) {
-    const id = btn.id;
-    alert(id);
-  } else {
+  const id = btn.id;
+  let value;
+  if (id.length === 0) {
+    value = btn.textContent;
     calcDisplay.textContent === "0"
       ? (calcDisplay.textContent = value)
       : (calcDisplay.textContent += value);
+  } else {
+    if (validFunctions.includes(id)) {
+      switch (id) {
+        case "clear":
+          clearCalc();
+          break;
+      }
+    }
+    if (validOperators.includes(id)) {
+      operator = id;
+
+      switch (id) {
+        case "add":
+          getNum();
+          console.log(numX, numY);
+          break;
+      }
+    }
   }
 }
 
-// grab calc buttons and add event listener
-const buttonContainer = document.querySelector(".calc-buttons");
-const buttons = buttonContainer.querySelectorAll("button");
-for (const btn of buttons) {
-  btn.addEventListener("click", () => useButton(btn));
+function clearCalc() {
+  calcDisplay.textContent = "0";
+  calcSavedNum.textContent = "";
+  numX;
+  numY;
+  operator;
+}
+
+function getNum() {
+  if (!numX) {
+    numX = Number(calcDisplay.textContent);
+    calcSavedNum.textContent = numX;
+    return numX;
+  } else {
+    numY = Number(calcDisplay.textContent);
+    return numY;
+  }
 }
